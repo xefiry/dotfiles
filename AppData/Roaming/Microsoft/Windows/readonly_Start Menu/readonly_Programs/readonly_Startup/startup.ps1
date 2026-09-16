f69e5aebc3a210e2b1d5ff94c1d34a59d54dc30f
xefiry <188508735+xefiry@users.noreply.github.com>
2026-09-16 08:32:30 +0200

    startup.ps1 improvements

--- AppData/Roaming/Microsoft/Windows/readonly_Start Menu/readonly_Programs/readonly_Startup/startup.ps1
+++ AppData/Roaming/Microsoft/Windows/readonly_Start Menu/readonly_Programs/readonly_Startup/startup.ps1
@@ -1,7 +1,12 @@
-try {
-  chezmoi update
-}
-catch {
-  Write-Host "Error caught : $LASTEXITCODE - $Error"
+Write-Host ">>> seek_and_destroy"
+seek_and_destroy
+
+Write-Host "
+>>> chezmoi update"
+chezmoi update 2> $null
+if (!($?)) {
+  Write-Error "chezmoi update - error, restart this script"
   Pause
+} else {
+  Start-Sleep -Seconds 5
 }
